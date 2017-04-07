3193edc0f43002deb64b215845b6b82dafe98ef0
John Gorman <johngorm@usc.edu>
2017-04-07 18:21:17 -0500

    Display Spotify song info in terminal

--- liri.js
+++ liri.js
@@ -38,21 +38,39 @@ function parseInstruction(instruction) {
 	else if(instruction === 'spotify-this-song'){
 		var song;
 		if(process.argv[3]){
-			song = process.argv[3];
+			song = process.argv[3].replace(/ /g,'+');
+			
 		}
 		else{
-			song = 'The Sign';
+			song = 'The+Sign+Ace+of+Base';
 		}
 		searchOptions = {
 			type: 'track',
-			query: song
+			query: song,
+			limit: 3
+
 		};
 		spotify.search(searchOptions, function(error, song){
 			if (error){
 				console.log('Error occured: ' + error)
 			}
 			else{
-				console.log(JSON.parse(JSON.stringify(song.tracks)));
+				var track = song.tracks.items[0];
+					var track_artists = track.artists;
+					var artists = '';
+					track_artists.forEach(function(artist){
+						artists = artists + artist.name + ', ';
+					});
+					console.log('Artist(s): ', artists);
+					console.log('Track name: ' , track.name);
+					console.log('Album name: ', track.album.name)
+					console.log('Song Preview Link: ', track.preview_url)
+					console.log('~~~~~~~~~~~~~~~~~~~~');
+				
+				// var tracks_info = song.tracks.items
+				// tracks_info.forEach(function(resource){
+				// 	console.log(resource)
+				// })
 			}
 		});
 	}
