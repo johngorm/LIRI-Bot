160aec87e3bbb108f5ae9668555f01cedecb03b7
John Gorman <johngorm@usc.edu>
2017-04-07 16:29:42 -0500

    Spotify track search implemented

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 //Node file that emulates SIRI, only using written language as input - hence LIRI
 console.assert(process.argv[2], 'No instruction provided in command line. ');
 var userCommand = process.argv[2];
+
 var twitter = require('twitter');
 var twitter_exports = require('./keys.js');
 var twitterKeys = twitter_exports.twitterKeys;
@@ -14,26 +15,46 @@ var user = {screen_name: 'stormy_eye'}
 
 var spotify = require('spotify');
 
+
 parseInstruction(userCommand);
 
 function parseInstruction(instruction) {
 	if(instruction === 'my-tweets'){
 		
 		client.get('statuses/user_timeline', user, function (error, tweets, response){
-			if(!error){
+			if(error){
+				console.log('Error occured: ' + error);
+				
+			}
+			else{
 				tweets.forEach(function(tweet) {
 					console.log(tweet.text.trim())
 				});
-			}
-			else{
-				console.log(error);
 			}
 		})
 		
 	}
 
 	else if(instruction === 'spotify-this-song'){
-		console.log('bar')
+		var song;
+		if(process.argv[3]){
+			song = process.argv[3];
+		}
+		else{
+			song = 'The Sign';
+		}
+		searchOptions = {
+			type: 'track',
+			query: song
+		};
+		spotify.search(searchOptions, function(error, song){
+			if (error){
+				console.log('Error occured: ' + error)
+			}
+			else{
+				console.log(JSON.parse(JSON.stringify(song.tracks)));
+			}
+		});
 	}
 
 	else if(instruction === 'movie-this'){
