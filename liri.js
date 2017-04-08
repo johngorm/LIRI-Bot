f050733ddbd97c9292212d9cd3eaf9e35025da45
John Gorman <johngorm@usc.edu>
2017-04-08 13:27:34 -0500

    Allow user to enter 3rd CLI option w/o quotes

--- liri.js
+++ liri.js
@@ -11,72 +11,86 @@ var client = new twitter({
 	access_token_key: twitterKeys.access_token_key,
 	access_token_secret: twitterKeys.access_token_secret
 });
-var user = {screen_name: 'stormy_eye'}
+
 
 var spotify = require('spotify');
 
+var request = require('request');
 
 parseInstruction(userCommand);
 
 function parseInstruction(instruction) {
 	if(instruction === 'my-tweets'){
 		
-		client.get('statuses/user_timeline', user, function (error, tweets, response){
+		client.get('statuses/user_timeline', {screen_name: 'stormy_eye'}, function (error, tweets, response){
 			if(error){
 				console.log('Error occured: ' + error);
-				
 			}
+
 			else{
 				tweets.forEach(function(tweet) {
 					console.log(tweet.text.trim())
 				});
 			}
-		})
+		});
 		
 	}
 
 	else if(instruction === 'spotify-this-song'){
-		var song;
+		var song = 'The+Sign+Ace+of+Base';
 		if(process.argv[3]){
-			song = process.argv[3].replace(/ /g,'+');
-			
+			song = process.argv.splice(3).join('+');
 		}
-		else{
-			song = 'The+Sign+Ace+of+Base';
-		}
+	
 		searchOptions = {
 			type: 'track',
-			query: song,
-			limit: 3
-
+			query: song
 		};
+
 		spotify.search(searchOptions, function(error, song){
 			if (error){
 				console.log('Error occured: ' + error)
 			}
 			else{
 				var track = song.tracks.items[0];
-					var track_artists = track.artists;
-					var artists = '';
-					track_artists.forEach(function(artist){
-						artists = artists + artist.name + ', ';
-					});
-					console.log('Artist(s): ', artists);
-					console.log('Track name: ' , track.name);
-					console.log('Album name: ', track.album.name)
-					console.log('Song Preview Link: ', track.preview_url)
-					console.log('~~~~~~~~~~~~~~~~~~~~');
-				
-				// var tracks_info = song.tracks.items
-				// tracks_info.forEach(function(resource){
-				// 	console.log(resource)
-				// })
+				var track_artists = track.artists;
+				var artists = '';
+				track_artists.forEach(function(artist){
+					artists = artists + artist.name + ', ';
+				});
+				console.log('\nArtist(s): ', artists);
+				console.log('Track name: ' , track.name);
+				console.log('Album name: ', track.album.name)
+				console.log('Track Preview Link: ', track.preview_url)
+				console.log('~~~~~~~~~~~~~~~~~~~~');
 			}
 		});
 	}
 
 	else if(instruction === 'movie-this'){
-		console.log('moo');
+		var movie = 'Mr.+Nobody';
+		if(process.argv[3]){
+			movie = process.argv.splice(3).join('+');
+		}
+
+		request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json', (error,response,body) => {
+			if(error || response.statusCode !== 200){
+				console.log('Error ' + response.statusCode + ': ' + error);
+			}
+			console.log(JSON.stringify(JSON.parse(body), null, 2))
+			// var movie = JSON.parse(body);
+			// //console.log(movie);
+			// console.log('Title: ', movie.Title);
+			// console.log('Year: ' , movie.Year);
+			// console.log('IMDB rating: ', movie.imdbRating);
+			// console.log('Country: ', movie.Country);
+			// console.log('Plot: ', movie.Plot);
+			// console.log('Staring: ', movie.Actors);
+
+		
+		})
+
+
 
 	}
 
