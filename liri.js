769531179cda75f906e4b15f6ed0b45a7b84d7db
John Gorman <johngorm@usc.edu>
2017-04-09 11:28:18 -0500

    Refactor quiries into individual functions

--- liri.js
+++ liri.js
@@ -1,39 +1,26 @@
 //Node file that emulates SIRI, only using written language as input - hence LIRI
 console.assert(process.argv[2], 'No instruction provided in command line. ');
-var userCommand = process.argv[2];
+const userCommand = process.argv[2];
 
-var twitter = require('twitter');
-var twitter_exports = require('./keys.js');
-var twitterKeys = twitter_exports.twitterKeys;
-var client = new twitter({
+const twitter = require('twitter');
+const twitter_exports = require('./keys.js');
+const twitterKeys = twitter_exports.twitterKeys;
+const client = new twitter({
 	consumer_key: twitterKeys.consumer_key,
 	consumer_secret: twitterKeys.consumer_secret,
 	access_token_key: twitterKeys.access_token_key,
 	access_token_secret: twitterKeys.access_token_secret
 });
 
+const spotify = require('spotify');
 
-var spotify = require('spotify');
-
-var request = require('request');
+const request = require('request');
 
 parseInstruction(userCommand);
 
 function parseInstruction(instruction) {
 	if(instruction === 'my-tweets'){
-		
-		client.get('statuses/user_timeline', {screen_name: 'stormy_eye'}, function (error, tweets, response){
-			if(error){
-				console.log('Error occured: ' + error);
-			}
-
-			else{
-				tweets.forEach(function(tweet) {
-					console.log(tweet.text.trim())
-				});
-			}
-		});
-		
+		queryTwitter();		
 	}
 
 	else if(instruction === 'spotify-this-song'){
@@ -41,30 +28,7 @@ function parseInstruction(instruction) {
 		if(process.argv[3]){
 			song = process.argv.splice(3).join('+');
 		}
-	
-		searchOptions = {
-			type: 'track',
-			query: song
-		};
-
-		spotify.search(searchOptions, function(error, song){
-			if (error){
-				console.log('Error occured: ' + error)
-			}
-			else{
-				var track = song.tracks.items[0];
-				var track_artists = track.artists;
-				var artists = '';
-				track_artists.forEach(function(artist){
-					artists = artists + artist.name + ', ';
-				});
-				console.log('\nArtist(s): ', artists);
-				console.log('Track name: ' , track.name);
-				console.log('Album name: ', track.album.name)
-				console.log('Track Preview Link: ', track.preview_url)
-				console.log('~~~~~~~~~~~~~~~~~~~~');
-			}
-		});
+		querySpotify(song);
 	}
 
 	else if(instruction === 'movie-this'){
@@ -72,25 +36,7 @@ function parseInstruction(instruction) {
 		if(process.argv[3]){
 			movie = process.argv.splice(3).join('+');
 		}
-
-		request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json', (error,response,body) => {
-			if(error || response.statusCode !== 200){
-				console.log('Error ' + response.statusCode + ': ' + error);
-			}
-			console.log(JSON.stringify(JSON.parse(body), null, 2))
-			// var movie = JSON.parse(body);
-			// //console.log(movie);
-			// console.log('Title: ', movie.Title);
-			// console.log('Year: ' , movie.Year);
-			// console.log('IMDB rating: ', movie.imdbRating);
-			// console.log('Country: ', movie.Country);
-			// console.log('Plot: ', movie.Plot);
-			// console.log('Staring: ', movie.Actors);
-
-		
-		})
-
-
+		queryOMDB(movie);
 
 	}
 
@@ -104,4 +50,69 @@ function parseInstruction(instruction) {
 		return -1;
 	}
 };
+
+function queryTwitter(){
+	client.get('statuses/user_timeline', {screen_name: 'stormy_eye'}, function (error, tweets, response){
+		if(error){
+			console.log('Error occured: ' + error);
+		}
+
+		else{
+			tweets.forEach(function(tweet) {
+				console.log('\n' + tweet.text.trim());
+			});
+		}
+	});
+};
+
+function querySpotify(track){
+	searchOptions = {
+		type: 'track',
+		query: track
+	};
+
+	spotify.search(searchOptions, function(error, song){
+		if (error){
+			console.error('Error occured: ' + error)
+		}
+		else{
+			let track = song.tracks.items[0];
+			let track_artists = track.artists;
+			var artists = '';
+			track_artists.forEach(function(artist){
+				artists = artists + artist.name + ', ';
+			});
+			console.log('\nArtist(s): ', artists);
+			console.log('Track name: ' , track.name);
+			console.log('Album name: ', track.album.name)
+			console.log('Track Preview Link: ', track.preview_url)
+			console.log('~~~~~~~~~~~~~~~~~~~~');
+		}
+	});
+};
+
+function queryOMDB(movieTitle){
+	request('http://www.omdbapi.com/?t=' + movieTitle + '&y=&plot=short&r=json', (error,response,body) => {
+		if(error || response.statusCode !== 200){
+			console.error('Error ' + response.statusCode + ': ' + error);
+		}
+		console.log(JSON.stringify(JSON.parse(body), null, 2))
+		let movieInfo = JSON.parse(body);
+		//console.log(movie);
+		console.log('\nTitle: ', movieInfo.Title);
+		console.log('Year: ' , movieInfo.Year);
+		console.log('IMDB rating: ', movieInfo.imdbRating);
+		console.log('Country: ', movieInfo.Country);
+		console.log('Plot: ', movieInfo.Plot);
+		console.log('Staring: ', movieInfo.Actors);
+		movieInfo.Ratings.forEach((rating) => {
+			if(rating['Source'] === 'Rotten Tomatoes'){
+				console.log('Rotten Tomatoes Rating:', rating['Value']);
+			}
+		})
+
+	
+	})
+
+}
 
