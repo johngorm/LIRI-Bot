a3b5c784ca00a6452e52d47a4eb8e36fa480d66f
John Gorman <johngorm@usc.edu>
2017-04-09 22:10:34 -0500

    Add keys.js to .gitignore

--- keys.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports.twitterKeys = {
-  consumer_key: 'KCsgEtBZxhO5pq0mKtxvssASz',
-  consumer_secret: 'sHGEPbYDH6sbJWqumlOAsAF9iuDSULfKSTjhQpYUNlBkhilKfe',
-  access_token_key: '844515910828351488-Vav3YfpevEHKrIZHvSP4ge3lCZuN1wx',
-  access_token_secret: 'lvg9tAawbAVCeY6qi0oEOpiYUSHcxD1txi5BPn5zErIcv',
-}
